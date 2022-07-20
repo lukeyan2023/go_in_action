4181f4cb9660a6408a4e850441e90b0fd1e561e9
luke <luke.yan@china-entercom.net>
2022-07-20 09:16:31 +0800

    add bookstore httpserver demo

---
+++ module_mode/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Println("hello, go module mode")
+	logrus.Println(uuid.NewString())
+}
